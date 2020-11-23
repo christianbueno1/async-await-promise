1dddbc0ef4669afd6c4a524e19e42067ca469efa
Joe Biden <joe@localhost.localdomain>
2020-11-22 22:22:03 -0500

    genesis

---
+++ scripts/js-frecodecamp-full-course-2018-12.js
@@ -0,0 +1,32 @@
+let fruits = [
+    {
+        'name': 'kiwi',
+        'weight': 76,
+        'unit of weight': 'gram',
+        'price': 3.5,
+        'currency': 'USD',//USD, EUR, GBP, INR
+    },
+    {
+        'name': 'cherry',
+        'weight': 12,
+        'unit of weight': 'g',
+        'price': 2.75,
+        'currency': 'EUR',//USD, EUR, GBP, INR
+    },
+    {
+        'name': 'mango',
+        'weight': 150,
+        'unit of weight': 'g',
+        'price': 5.25,
+        'currency': 'USD',//USD, EUR, GBP, INR
+    },
+
+];
+let x = 'message here';
+let answer = x || 'No such property';
+console.log(answer);
+// console.log("hello");
+
+function lookUpFruit (name, prop) {
+
+}
